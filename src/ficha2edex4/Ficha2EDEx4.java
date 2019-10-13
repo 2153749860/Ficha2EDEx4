19b2f9b3a37d7dbf8a9fa686df735ad6cd268c77
2153749860 <tiagorocha1515@gmail.com>
2019-10-13 15:36:25 +0100

    Metodo add adicionado

--- src/ficha2edex4/Ficha2EDEx4.java
+++ src/ficha2edex4/Ficha2EDEx4.java
@@ -16,7 +16,19 @@ public class Ficha2EDEx4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Pessoa pessoa1 = new Pessoa("Tiago", 20);
+        Pessoa pessoa2 = new Pessoa("Vitor", 21);
+        Pessoa pessoa3 = new Pessoa("Carlos", 21);
+        Pessoa pessoa4 = new Pessoa("Joao", 21);
         
+        DoublyLinkedList<Pessoa> lista = new DoublyLinkedList<>();
+        
+        lista.add(pessoa1);
+        lista.add(pessoa2);
+        lista.add(pessoa3);
+        lista.add(pessoa4);
+        
+        lista.print();
     }
     
 }
